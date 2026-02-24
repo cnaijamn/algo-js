0f81b9216666eb22d9f214424e23b1e66064fcd7
Chez Naijamn <cnaijamn@gmail.com>
2026-02-25 07:47:30 +0900

    追加

---
+++ src/etc/kuku.mjs
@@ -0,0 +1,14 @@
+/*
+ * 九九
+ */
+export default function kuku() {
+  	const row = [];
+  	for (let i = 2; i <= 9; i++) {
+    	const col = [];
+    	for (let j = 1; j <= 9; j++) {
+      		col.push(i * j);
+    	}
+    	row.push(col);
+  	}
+  	return row;
+}
