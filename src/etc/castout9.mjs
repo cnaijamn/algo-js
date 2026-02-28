7a0d32c80437b00c3a65cc833a6e890980530f2d
Chez Naijamn <cnaijamn@gmail.com>
2026-02-28 10:39:23 +0900

    修正

--- src/etc/castout9.mjs
+++ src/etc/castout9.mjs
@@ -1,15 +1,38 @@
 /**
  * 九去法.
- * @param {bigint} n - 数値
+ * @param {number|bigint} n - 整数値
  * @return {bigint} 残りの数
  */
 export default function castout9(n) {
-  if (typeof n !== "bigint") {
-    throw new TypeError('n must be bigint greater than or equal to zero');
+  if (!((Number.isInteger(n) || typeof n === 'bigint') && n >= 0)) {
+    throw new Error('n must be an integer greater than or equal to zero');
   }
-  if (n < 0n) {
-    throw new RangeError('n must be bigint greater than or equal to zero');
+  if (typeof n === 'number') {
+    return _by_number(n);
+  } else {
+    return _by_bignum(n);
   }
+}
+
+function _by_number(n) {
+  if (n > Number.MAX_SAFE_INTEGER) {
+    throw new RangeError(`n must be an integer less than or equal to ${Number.MAX_SAFE_INTEGER}`);
+  }
+  while (n >= 9) {
+    let x = n;
+    let sum = 0;
+    while (x > 0) {
+      let c = x % 10;
+      x = Math.trunc(x / 10);
+      if (c == 9 || c == 0) continue;
+      sum += c;
+    }
+    n = sum;
+  }
+  return n;
+}
+
+function _by_bignum(n) {
   while (n >= 9n) {
     let x = n;
     let sum = 0n;
