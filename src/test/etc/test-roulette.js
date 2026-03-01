0b9504dde4070dccdabc132f651fd7e965cbe544
Chez Naijamn <cnaijamn@gmail.com>
2026-03-01 18:47:47 +0900

    追加

--- src/test/etc/test-roulette.js
+++ src/test/etc/test-roulette.js
@@ -2,11 +2,11 @@ import assert from 'node:assert';
 import roulette from '../../etc/roulette.mjs';
 
 const dat = [
-  { name: 'a', value:  3 },
-  { name: 'b', value: 10 },
+  { name: 'a', value: 10 },
+  { name: 'b', value: 20 },
   { name: 'c', value: 30 },
-  { name: 'd', value: 13 },
-  { name: 'e', value: 70 },
+  { name: 'd', value: 70 },
+  { name: 'e', value: 40 },
 ];
 let ret = roulette(dat);
 let x = null;
@@ -18,3 +18,13 @@ for (let i of dat) {
   }
 }
 assert.ok(x !== null);
+
+const N = 1000000;
+const A = new Float64Array(dat.length);
+for (let i = 0; i < N; i++) {
+  const ret = roulette(dat);
+  const idx = dat.findIndex(e => e.name === ret.name);
+  ++A[idx];
+}
+let x1 = A[0] / dat[0].value;
+console.log(A.map(e => e / x1));
